adae9026ffa95dcec662cd7885d5d2f9e84b3b04
irinababunova <xleyla@gmail.com>
2023-06-08 18:45:39 +0300

    add fixed code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,23 +11,29 @@ public class Main {
 
         System.out.println("Простая сумма");
 
-        /** Посчитать сумму геометрической прогрессии для чисел от 0 до 100 */
-        int sum=0;
-        for (int n=0; n<=100; n++) {
-            sum=sum+n;
-        }
-        System.out.println("Сумма= " + sum);
+        /** Посчитать сумму геометрической прогрессии для чисел от 0 до 100 ? как можно считать сумму геометрической прогрессии
+         * если начинается с 0? Это меня и смутило */
+
+        int sumGeomPr=0;
+        int q=2;
+        int n=5;
+        sumGeomPr=sumGeomPr+n;
+        while (n<=100) {
+            n=n*q;
+            sumGeomPr=sumGeomPr+n;
+        } System.out.println("Сумма геометрической прогрессии = " + sumGeomPr);
+
 
         System.out.println("Странная сумма");
         /** Для чисел от 1 до 100 посчитать сколько получится, если писать 1 - 2 + 3 - 4 и так далее.*/
-        int summa=0;
+        int sum=0; /* переменная так названа, потому что есть уже задача с такой переменной.*/
         for (int a=1; a<=100; a++) {
             if (a%2!=0) {
-                summa=summa+a;
+                sum=sum+a;
             } else {
-                summa=summa-a;
+                sum=sum-a;
             }
-        } System.out.println("Сумма " + summa);
+        } System.out.println("Сумма " + sum);
 
         System.out.println("Знаменитый fizz buzz");
         /** Написать программу, которая для каждой цифры в диапазоне от 0 до 100 печатает следующии
@@ -36,30 +42,35 @@ public class Main {
          Если цифра делится и на три и на пять - печатает FizzBuzz */
 
         for (int number=0; number<=100; number++) {
-            if ((number%3==0) && (number%5==0)) {
+            if ((number% 3 == 0) && (number % 5 == 0)) {
                 System.out.println(number + ": FizzBuzz");
                 continue;
-            } else if (number%3==0) {
+            } if (number % 3 == 0) {
                 System.out.println(number + ": fizz");
                 continue;
-            } else if (number%5==0) {
+            } if (number % 5 == 0) {
                 System.out.println(number + ": buzz");
                 continue;
-            } System.out.println(number + ":");
+            }// System.out.println(number + ":");
         }
 
         System.out.println("Простые числа");
         /** Простое число это такое число, которое не делится ни на какое другое число нацело.
          Написать программу, которая будет определять простое число в переменной или нет. */
 
-        int simpleNumber=67;
-        for (int index=2; index<simpleNumber; index++){
-            if (simpleNumber%index==0) {
-                System.out.println(simpleNumber + " Число сложное");
+
+        int simpleNumber=27;
+        int ind=2;
+        boolean simple=false;
+        while (ind<simpleNumber) {
+            if (simpleNumber % ind == 0) {
+                simple = true;
                 break;
-            } else System.out.println(simpleNumber + " Число простое");
-                   break;
-        }
+            } ind++; }
+        if (!simple) {
+            System.out.println(simpleNumber + " - Простое число");
+        } else System.out.println(simpleNumber + " - Составное число");
+
 
         System.out.println("Запасы гречки");
         /** Программист Василий очень любит гречку. Поэтому он заготовил запас гречки на годы вперёд
@@ -75,18 +86,43 @@ public class Main {
 
          int buckwheatStock=100;
          int costOneKilogram=100;
-         int costAllKilogram;
-         int foodEaten=8;
+         int costAllKilogramOneMonth;
+         int foodEaten=6;
+         int sumCostForAllMonths=0;
 
-         /**while (buckwheatStock>=foodEaten) {
-             costAllKilogram=buckwheatStock*costOneKilogram;
-             System.out.println(costAllKilogram);
+         while (buckwheatStock>=0) {
+             costAllKilogramOneMonth=buckwheatStock*costOneKilogram;
+             //System.out.println(costAllKilogramOneMonth);
+             sumCostForAllMonths=sumCostForAllMonths+costAllKilogramOneMonth;
              buckwheatStock=buckwheatStock-foodEaten;
-         }*/
-         do { costAllKilogram=buckwheatStock*costOneKilogram;
-             System.out.println(costAllKilogram);
-             buckwheatStock=buckwheatStock-foodEaten;
-         } while (buckwheatStock>=foodEaten);
+         }
+        System.out.println("Всего Василию нужно заплатить " + sumCostForAllMonths);
+
+        System.out.println("Запасы гречки *");
+        /** Программист Василий очень любит гречку. Поэтому он заготовил запас гречки на годы вперёд
+         который хранится на складе.
+         В его запасе 100 килограммов гречки. Хранение одного килограмма в месяц стоит 100 рублей.
+         Плата вносится за месяц вперёд. За месяц Василий съедает примерно 6 килограммов гречки.
+         Каждый месяц Василий платит за хранение остатка. То есть в первый месяц от заплатит за 100
+         килограммов, второй месяц за 94 и так далее, пока гречка не закончится.
+         Написать программу, которая посчитает сколько денег нужно будет Василию, чтобы оплатить
+         хранение гречки, до того момента, как он её съест.
+         Со звёздочкой. В начале задавать в переменной не сколько килограммов запас Василий, а в
+         течение какого срока он хочет питаться гречкой и рассчитывать остальное исходя из этого. */
+
+        int periodYear=2;
+        int costOneKilogram2=100;
+        int costAllKilogramOneMonth2;
+        int foodEaten2=6;
+        int sumCostForAllMonths2=0;
+
+        int countMonths=periodYear*12;
+        int totalBuckwheat=countMonths*foodEaten2;
+        for (int month=1; month<=countMonths; month++){
+            costAllKilogramOneMonth2=totalBuckwheat*costOneKilogram2;
+            sumCostForAllMonths2=sumCostForAllMonths2+costAllKilogramOneMonth2;
+            totalBuckwheat=totalBuckwheat-foodEaten2;
+        } System.out.println("Всего Василию нужно заплатить " + sumCostForAllMonths2);
 
     }
 }
